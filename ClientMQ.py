71eb170d8f63f25cdcba6836c954dd45c97b6063
lucasmaia123 <leonziin321@gmail.com>
2023-12-11 13:38:49 -0300

    first commit

---
+++ ClientMQ.py
@@ -0,0 +1,359 @@
+import Pyro4
+import threading
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
+from tkinter import ttk
+from sys import exit
+
+Pyro4.config.SERIALIZERS_ACCEPTED = {'serpent', 'marshal'}
+daemon = Pyro4.Daemon()
+server = None
+
+def threaded(func):
+    def wrapper(*args, **kwargs):
+        return threading.Thread(target=func, args=args, kwargs=kwargs).start()
+    return wrapper
+
+@threaded
+def listen2server():
+    daemon.requestLoop()
+
+class Topic(tk.Toplevel):
+    def __init__(self, master, name):
+        self.master = master
+        self.name = name
+        self.open = False
+        self.pendent_messages = 0
+
+    def open_topic(self):
+        if self.open:
+            return
+        else:
+            self.open = True
+        
+        super().__init__(self.master, height=400, width=400)
+        tk.Label(self, text=f'Tópico {self.name}').pack(padx=20, pady=10)
+        tk.Button(self, text='Listar membros', command=self.list_members).pack(side='right', padx=20, pady=10)
+
+        self.textBox = ScrolledText(self, wrap=tk.WORD, state='disabled')
+        self.textBox.config(height=20, width=50)
+        self.textBox.pack(padx=10, pady=10)
+
+        tk.Label(self, text='Send a message:').pack(padx=5)
+        self.entry = tk.Entry(self, width=50)
+        self.entry.pack(side='right', padx=5, pady=5)
+        self.entry.bind('<Return>', lambda e: self.send_message())
+        self.entry.focus()
+
+        server.open_connection(self.name)
+        self.pendent_messages = 0
+        self.protocol('WM_DELETE_WINDOW', self.close_window)
+
+    def list_members(self):
+        window = tk.Toplevel(self.master)
+        members = server.get_topics[self.name]
+        if len(members) < 2:
+            tk.Label(window, text='Você é o único membro!').pack(padx=10, pady=10)
+            return
+        counter = 0
+        for member in members:
+            tk.Label(window, text=f'{member}').grid(row=counter, column=0, padx=5, pady=5)
+            tk.Button(window, text='Adicionar contato', command=lambda: client.add_contact(member, 'client')).grid(row=counter, column=1)
+            counter+=1
+
+    def send_message(self, event = None):
+        msg = self.entry.get()
+        if msg:
+            self.entry.delete(0, tk.END)
+            server.send_message(msg, self.name, 'topic')
+
+    def message(self, msg):
+        self.textBox.config(state='normal')
+        self.textBox.insert(tk.END, msg + '\n')
+        self.textBox.see('end')
+        self.textBox.config(state='disabled')
+
+    def close_window(self):
+        server.close_connection(self.name)
+        self.open = False
+        self.destroy()
+        client.update()
+
+class Contact(tk.Toplevel):
+    def __init__(self, master, name):
+        self.name = name
+        self.master = master
+        self.open = False
+        self.pendent_messages = 0
+
+    def start_conversation(self):
+        if self.open:
+            return
+        else:
+            self.open = True
+
+        super().__init__(self.master, height=400, width=400)
+
+        tk.Label(self, text=f'Conversa com {self.name}').pack(padx=10, pady=10)
+        self.scrollText = ScrolledText(self, wrap=tk.WORD, state='disabled')
+        self.scrollText.config(height=20, width=50)
+        self.scrollText.pack(padx=10, pady=10)
+
+        tk.Label(self, text='Digite uma mensagem:').pack(padx=5)
+        self.entry = tk.Entry(self, width=50)
+        self.entry.pack(side='right', padx=5, pady=5)
+        self.entry.bind('<Return>', lambda e: self.send_message())
+
+        server.open_connection(self.name)
+        self.pendent_messages = 0
+        self.protocol('WM_DELETE_WINDOW', self.close_window)
+
+    def send_message(self, event = None):
+        msg = self.entry.get()
+        if msg:
+            self.entry.delete(0, tk.END)
+            server.send_message(msg, self.name, 'contact')
+            self.message(f'{client.name}: {msg}')
+
+    def message(self, msg):
+        if msg:
+            self.scrollText.config(state='normal')
+            self.scrollText.insert(tk.END, msg + '\n')
+            self.scrollText.see('end')
+            self.scrollText.config(state='disabled')
+
+    def close_window(self):
+        server.close_connection(self.name)
+        self.open = False
+        self.destroy()
+        client.update()
+
+class Main_menu(tk.Frame):
+
+    contacts = {}
+    notify_list = {}
+
+    @Pyro4.expose
+    def get_contacts(self):
+        return self.contacts
+
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry('200x150')
+        super().__init__(master)
+        self.pack(fill='both')
+
+        self.text = tk.Label(self, text='Conectando ao servidor...')
+        self.text.pack(padx=10, pady=10)
+        self.get_server()
+
+    @threaded
+    def get_server(self):
+        global server
+        try:
+            ns = Pyro4.locateNS()
+        except:
+            self.message_popup('[ERROR] Servidor de nomes não iniciado!')
+            return
+        server = Pyro4.Proxy(ns.lookup('Server'))
+
+        self.text.config(text='Digite o seu nome: ')
+        entry = tk.Entry(self, width=30)
+        entry.pack(pady=10)
+        self.login_button = tk.Button(self, text='Entrar', command=lambda: self.login(entry.get()))
+        self.login_button.pack(side='bottom', pady=10)
+        entry.bind('<Return>', lambda event: self.login(entry.get()))
+        entry.focus()
+
+    def login(self, name, event = None):
+        global server
+        if name:
+            self.name = name        
+        else:
+            return
+        tk.Label(self, text='Logando...').pack(side='bottom', pady=10)
+        self.login_button.config(state='disabled')
+        uri = daemon.register(self)
+        status = server.start(self.name, uri)
+        if status:
+            self.destroy()
+            self.master.geometry('500x400')
+            self.master.title(self.name)
+            self.draw_client_window()
+            server.notificationQueue()
+            self.master.protocol('WM_DELETE_WINDOW', self.disconnect)
+        else:
+            self.message_popup(f'Cliente {name} já está logado!')
+
+    @threaded
+    def draw_client_window(self):
+        super().__init__(self.master, height=400, width=500)
+        self.pack(fill='x', expand=True)
+
+        self.scrollBar = tk.Scrollbar(self, orient='vertical')
+        self.scrollBar.pack(side='right', fill='y')
+
+        self.canvas = tk.Canvas(self, width=450, height=300)
+        self.canvas.pack(fill='x')
+
+        self.scrollBar.configure(command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.scrollBar.set)
+
+        tk.Button(self, text='Novo Contato', command=self.add_contact_window).pack(side='left', padx=20, pady=10)
+        tk.Button(self, text='Criar Grupo', command=self.create_topic_window).pack(side='left', padx=20, pady=10)
+        tk.Button(self, text='Update', command=self.update).pack(side='left', padx=20, pady=10)
+
+        self.draw_client_frame()
+
+    def draw_client_frame(self):
+        self.scrollFrame = tk.Frame(self.canvas, bg='white')
+        self.scrollFrame.pack(fill='both', expand=True)
+        self.scrollFrame.bind('<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox('all')))
+        self.frame = self.canvas.create_window((0, 0), window=self.scrollFrame, anchor='nw')
+
+        tk.Label(self.scrollFrame, text='Contato').grid(row=0, column=0, padx=5, pady=5)
+        tk.Label(self.scrollFrame, text='Tipo').grid(row=0, column=1, padx=5, pady=5)
+
+        if self.contacts == {}:
+            tk.Label(self.scrollFrame, text='Você não tem contatos!').grid(row=1, column=0, padx=5, pady=5)
+        else:    
+            counter = 0
+            for contact in self.contacts.values():
+                counter += 1
+                if isinstance(contact, Contact):
+                    tk.Label(self.scrollFrame, text=contact.name).grid(row=counter, column=0, padx=5, pady=5)
+                    tk.Label(self.scrollFrame, text='Contato').grid(row=counter, column=1, padx=5, pady=5)
+                    tk.Button(self.scrollFrame, text='Iniciar conversa', command=contact.start_conversation).grid(row=counter, column=2, padx=5, pady=5)
+                    tk.Button(self.scrollFrame, text='Excluir contato', command=lambda contact=contact: self.delete_contact(contact.name)).grid(row=counter, column=3, padx=5, pady=5)
+                elif isinstance(contact, Topic):
+                    tk.Label(self.scrollFrame, text=contact.name).grid(row=counter, column=0, padx=5, pady=5)
+                    tk.Label(self.scrollFrame, text='Grupo').grid(row=counter, column=1, padx=5, pady=5)
+                    tk.Button(self.scrollFrame, text='Entrar', command=contact.open_topic).grid(row=counter, column=2, padx=5, pady=5)
+                    tk.Button(self.scrollFrame, text='Sair do grupo', command=lambda contact=contact: self.delete_contact(contact.name)).grid(row=counter, column=3, padx=5, pady=5)
+                if contact.pendent_messages > 0:
+                    if contact.pendent_messages == 1:
+                        self.notify_list[contact] = tk.Label(self.scrollFrame, text='1 mensagem!')
+                    else:
+                        self.notify_list[contact] = tk.Label(self.scrollFrame, text=f'{contact.pendent_messages} mensagems!')
+                    self.notify_list[contact].grid(row=counter, column=4, padx=5, pady=5)
+
+    def update(self):
+        self.canvas.delete(self.frame)
+        self.scrollFrame.destroy()
+        self.draw_client_frame()
+
+    @Pyro4.expose
+    def notify(self, amount, address):
+        if not self.contacts[address].open and amount:
+            self.contacts[address].pendent_messages += amount
+            try:
+                if self.contacts[address].pendent_messages == 1:
+                    self.notify_list[address].config('1 mensagem!')
+                    self.update()
+                else:
+                    self.notify_list[address].config(f'{self.contacts[address].pendent_messages} mensagems!')
+                    self.update()
+            except:
+                pass
+
+    def add_contact_window(self):
+        radio_var = tk.StringVar()
+        window = tk.Toplevel(self.master, height=200, width=300)
+        tk.Label(window, text='Digite o nome do cliente/tópico:').grid(row=0, column=0, padx=5, pady=5)
+        ttk.Radiobutton(window, text='Cliente', variable=radio_var, value='client').grid(row=1, column=0, padx=5, pady=5)
+        ttk.Radiobutton(window, text='Tópico', variable=radio_var, value='topic').grid(row=1, column=1, padx=5, pady=5)
+        entry = tk.Entry(window, width=50)
+        entry.grid(row=2, column=0, padx=5, pady=5)
+        entry.bind('<Return>', lambda e: self.add_contact(entry.get(), radio_var.get(), window))
+        entry.focus()
+
+    def add_contact(self, name, type, window=None, event=None):
+        if name and type:
+            if name in self.contacts.keys():
+                self.message_popup('Você já tem esse contato!')
+                return
+            if type == 'client':
+                status = server.new_contact(name)
+                if status:
+                    self.contacts[name] = Contact(self.master, name)
+                    if window:
+                        window.destroy()
+                    self.update()
+                else:
+                    self.message_popup('Cliente não encontrado!')
+                    if window:
+                        window.destroy()
+            elif type == 'topic':
+                if name in server.get_topics.keys():
+                    server.new_topic(name)
+                    self.contacts[name] = Topic(self.master, name)
+                    if window:
+                        window.destroy()
+                    self.update()
+                else:
+                    self.message_popup('Tópico não encontrado!')
+                    if window:
+                        window.destroy()
+
+    # Função simplificada para carregar contato pelo servidor
+    @Pyro4.expose
+    def load_contact(self, name, type):
+        if type == 'client':
+            self.contacts[name] = Contact(self.master, name)
+        elif type == 'topic':
+            self.contacts[name] = Topic(self.master, name)
+
+    def create_topic_window(self):
+        window = tk.Toplevel(self.master, height=200, width=300)
+        tk.Label(window, text='Digite o nome do tópico a ser criado:').pack(padx=5, pady=5)
+        entry = tk.Entry(window, width=50)
+        entry.pack(padx=5, pady=5)
+        entry.bind('<Return>', lambda e: self.create_topic(entry.get(), window))
+        entry.focus()
+
+    def create_topic(self, name, window):
+        if name:
+            if name in server.get_topics.keys():
+                self.message_popup('Tópico já existe!')
+            else:
+                server.new_topic(name)
+                self.contacts[name] = Topic(self.master, name)
+                self.message_popup('Tópico criado com éxito!')
+                window.destroy()
+                self.update()
+
+    def delete_contact(self, name):
+        if self.contacts[name].open:
+            self.contacts[name].close_window()
+        if isinstance(self.contacts[name], Contact):
+            server.removeQueue(name, 'contact')
+        elif isinstance(self.contacts[name], Topic):
+            server.removeQueue(name, 'topic')
+        del self.contacts[name]
+        self.update()
+
+    @Pyro4.expose
+    @threaded
+    def redirect_message(self, msg, origin, topic = None):
+        if topic:
+            self.contacts[topic].message(f'{origin}: {msg}')
+        else:
+            self.contacts[origin].message(f'{origin}: {msg}')
+
+    @Pyro4.expose
+    def message_popup(self, msg):
+        popup = tk.Toplevel(self.master, height=200, width=300)
+        tk.Label(popup, text=msg).pack(padx=10, pady=10)
+        tk.Button(popup, text='Ok', command=popup.destroy).pack(side='bottom', pady=5)
+        popup.focus_force()
+
+    def disconnect(self):
+        server.logout()
+        self.master.quit()
+        self.master.destroy()
+
+root = tk.Tk()
+client = Main_menu(root)
+listen2server()
+root.mainloop()
+exit()
---
+++ ServerMQ.py
@@ -0,0 +1,212 @@
+import stomp
+import urllib3
+import urllib3.util
+import Pyro4
+import Pyro4.naming
+from random import choices
+import string
+import threading
+import json
+import sys
+
+clients = {}
+topics = {}
+
+def threaded(func):
+    def wrapper(*args, **kwargs):
+        return threading.Thread(target=func, args=args, kwargs=kwargs, daemon=True).start()
+    return wrapper
+
+def id_generator(size):
+    return ''.join(choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k=size))
+
+http = urllib3.PoolManager()
+headers = urllib3.make_headers(basic_auth='admin:admin')
+
+def load_server_data():
+    global clients, topics
+    addresses = http.request('GET', f'http://localhost:8161/console/jolokia/exec/org.apache.activemq.artemis:broker="0.0.0.0"/listAddresses(java.lang.String)/,', headers=headers).json()
+    addresses = addresses['value'].split(',')
+    for address in addresses[4:]:
+        address_info = http.request('GET', f'http://localhost:8161/console/jolokia/exec/org.apache.activemq.artemis:broker="0.0.0.0"/getAddressInfo/{address}', headers=headers).json()
+        address_info = address_info['value']
+        if 'ANYCAST' in address_info:
+            clients[address] = None
+        else:
+            topics[address] = []
+            response = http.request('GET', f'http://localhost:8161/console/jolokia/exec/org.apache.activemq.artemis:broker="0.0.0.0"/getQueueNames/MULTICAST', headers=headers).json()
+            topic_names = response['value']
+            for topic in topic_names:
+                topic = topic.split('.')
+                topic_name = topic[0]
+                if topic_name == address:
+                    topics[address].append(topic[1])
+            
+@threaded
+def start_ns():
+    print('Inicializando o servidor de nomes...')
+    Pyro4.naming.startNSloop(host='localhost', port=9090)
+
+class Listener(stomp.ConnectionListener):
+    def on_error(self, frame):
+        print(f'[ERROR] {frame.headers["Error"]}')
+    
+    def on_message(self, frame):
+        origin = frame.headers['name']
+        destination = frame.headers['target']
+        type = frame.headers['type']
+        if type == 'topic':
+            topic = frame.headers['topic']
+            clients[destination].redirect_message(frame.body, origin, topic)
+        elif type == 'contact':
+            clients[destination].redirect_message(frame.body, origin)
+        elif type == 'new contact':
+            clients[destination].message_popup(f'{origin} o adicionou como contato!')
+
+class Client:
+
+    @Pyro4.expose
+    def start(self, name, uri):
+        if name in clients.keys():
+            if clients[name] != None:
+                return False
+        else:
+            print(f'Criando nova conta para cliente {name}...')
+            q_json = json.dumps({"name": name, "address": name, "routing-type": "ANYCAST", "durable": True})
+            http.request('GET', f'http://localhost:8161/console/jolokia/exec/org.apache.activemq.artemis:broker="0.0.0.0"/createQueue(java.lang.String)/{q_json}', headers=headers).json()
+        self.name = name
+        self.id = id_generator(10)
+        self.client = Pyro4.Proxy(uri)
+        clients[self.name] = self.client
+        print(f'Cliente {self.name} se logou!')
+        self.load_client_contacts()
+        return True
+        
+    @Pyro4.expose
+    def notificationQueue(self):
+        conn.subscribe(self.name, id=self.name, headers={'subscription-type': 'ANYCAST', 'durable-subscription-name': self.name})
+
+    @Pyro4.expose
+    @property
+    def get_clients(self):
+        return clients
+    
+    @Pyro4.expose
+    @property
+    def get_topics(self):
+        return topics
+
+    def load_client_contacts(self):
+        response = http.request('GET', f'http://localhost:8161/console/jolokia/exec/org.apache.activemq.artemis:broker="0.0.0.0"/getQueueNames/ANYCAST', headers=headers).json()
+        client_queues = response['value']
+        for queue in client_queues:
+            queue = queue.split('.')
+            try:
+                if queue[0] == self.name:
+                    client_name = queue[1]
+                    if client_name in clients.keys():
+                        self.client.load_contact(client_name, 'client')
+                        p_mens = http.request('GET', f'http://localhost:8161/console/jolokia/exec/org.apache.activemq.artemis:broker="0.0.0.0",component=addresses,address="{self.name}",subcomponent=queues,routing-type="anycast",queue="{self.name}.{client_name}"/countMessages()', headers=headers).json()
+                        p_mens = p_mens['value']
+                        self.client.notify(p_mens, client_name)
+            except:
+                continue
+        for topic in topics.keys():
+            if self.name in topics[topic]:
+                self.client.load_contact(topic, 'topic')
+                p_mens = http.request('GET', f'http://localhost:8161/console/jolokia/exec/org.apache.activemq.artemis:broker="0.0.0.0",component=addresses,address="{topic}",subcomponent=queues,routing-type="multicast",queue="{topic}.{self.name}"/countMessages()', headers=headers).json()
+                p_mens = p_mens['value']
+                self.client.notify(p_mens, topic)
+
+    @Pyro4.expose
+    def open_connection(self, target):
+        if target in topics.keys():
+            conn.subscribe(f'{target}::{target}.{self.name}', id=f'{target}.{self.name}', headers={'subscription-type': 'MULTICAST', 'durable-subscription-name': self.name})
+        else:
+            conn.subscribe(f'{self.name}::{self.name}.{target}', id=f'{self.name}.{target}', headers={'subscription-type': 'ANYCAST', 'durable-subscription-name': self.name})
+
+    @Pyro4.expose
+    def close_connection(self, target):
+        if target in topics.keys():
+            conn.unsubscribe(f'{target}.{self.name}')
+        else:
+            conn.unsubscribe(f'{self.name}.{target}')
+
+    @Pyro4.expose
+    def removeQueue(self, name, type):
+        global topics
+        if type == 'contact':
+            http.request('GET', f'http://localhost:8161/console/jolokia/exec/org.apache.activemq.artemis:broker="0.0.0.0"/destroyQueue(java.lang.String)/{self.name}.{name}', headers=headers).json()
+            http.request('GET', f'http://localhost:8161/console/jolokia/exec/org.apache.activemq.artemis:broker="0.0.0.0"/destroyQueue(java.lang.String)/{name}.{self.name}', headers=headers).json()
+        elif type == 'topic':
+            http.request('GET', f'http://localhost:8161/console/jolokia/exec/org.apache.activemq.artemis:broker="0.0.0.0"/destroyQueue(java.lang.String)/{name}.{self.name}', headers=headers).json()
+            topics[name].remove(self.name)
+            if topics[name] == []:
+                http.request('GET', f'http://localhost:8161/console/jolokia/exec/org.apache.activemq.artemis:broker="0.0.0.0"/deleteAddress(java.lang.String,boolean)/{name}/true', headers=headers).json()
+                print(f'Tópico {name} foi deletado por não conter membros!')
+
+    @Pyro4.expose
+    def new_contact(self, name):
+        if name in clients.keys():
+            try:
+                clients[name].load_contact(self.name, 'contact')
+                clients[name].message_popup(f'{self.name} o adicionou como contato!')
+            except:
+                conn.send(name, self.name, headers={'persistent': True, 'name': self.name, 'target': name, 'type': 'new contact'})
+            q_json = json.dumps({"name": f"{self.name}.{name}", "address": self.name, "routing-type": "ANYCAST", "durable": True})
+            http.request('GET', f'http://localhost:8161/console/jolokia/exec/org.apache.activemq.artemis:broker="0.0.0.0"/createQueue(java.lang.String)/{q_json}', headers=headers).json()
+            q_json = json.dumps({"name": f"{name}.{self.name}", "address": name, "routing-type": "ANYCAST", "durable": True})
+            http.request('GET', f'http://localhost:8161/console/jolokia/exec/org.apache.activemq.artemis:broker="0.0.0.0"/createQueue(java.lang.String)/{q_json}', headers=headers).json()
+            return True
+        else:
+            return False
+        
+    @Pyro4.expose
+    def new_topic(self, name):
+        global topics
+        q_json = json.dumps({"name": f"{name}.{self.name}", "address": name, "routing-type": "MULTICAST", "durable": True})
+        http.request('GET', f'http://localhost:8161/console/jolokia/exec/org.apache.activemq.artemis:broker="0.0.0.0"/createQueue(java.lang.String)/{q_json}', headers=headers).json()
+        if name not in topics.keys():
+            topics[name] = []
+        topics[name].append(self.name)
+
+    @Pyro4.expose
+    def send_message(self, msg, target, type):
+        if type == 'contact':
+            conn.send(f'{target}::{target}.{self.name}', msg, headers={'persistent': True, 'name': self.name, 'target': target, 'type': type})
+            try:
+                clients[target].notify(1, self.name)
+            except:
+                pass
+        elif type == 'topic':
+            for client in topics[target]:
+                conn.send(f'{target}::{target}.{client}', msg, headers={'persistent': True, 'name': self.name, 'target': client, 'type': type, 'topic': target})
+                try:
+                    clients[client].notify(1, target)
+                except:
+                    pass
+
+    @Pyro4.expose
+    def logout(self):
+        conn.unsubscribe(self.name)
+        clients[self.name] = None
+        print(f'Cliente {self.name} se desconectou!')
+
+try:
+    conn = stomp.Connection([('localhost', 61613)], auto_content_length=False, heartbeats=(4000, 4000))
+except:
+    input('[ERROR] Broker não iniciado!')
+    sys.exit()
+conn.set_listener('', Listener())
+conn.connect('admin', 'admin', wait=True, headers={'client-id': 'admin'})
+
+start_ns()
+daemon = Pyro4.Daemon(host='localhost', port=8080)
+uri = daemon.register(Client)
+ns = Pyro4.locateNS()
+ns.register('Server', uri)
+load_server_data()
+print('Servidor inicializado!')
+print(f'clientes: {clients}')
+print(f'tópicos: {topics}')
+daemon.requestLoop()
